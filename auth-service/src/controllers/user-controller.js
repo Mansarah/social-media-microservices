cafd40395abde632bd7fa7858c0e9691d96db0d2
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-09-28 17:33:34 +0530

    `feat: auth-service/validation joi package`

--- auth-service/src/controllers/user-controller.js
+++ auth-service/src/controllers/user-controller.js
@@ -1,9 +1,18 @@
 
 
+const logger = require('../utils/logger')
 
 
 // user registration
 
+const registerUser = async(req,res)=>{
+    logger.info('Registration endpoint hit...')
+    try {
+        // validate the schema
+    } catch (e) {
+        
+    }
+}
 
 // user login 
 
