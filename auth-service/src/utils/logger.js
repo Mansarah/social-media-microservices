d0cf96b8d3738a9182723f18a91a68a8317265be
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-09-28 15:54:36 +0530

    `feat: auth-service/logger.js`

--- auth-service/src/utils/logger.js
+++ auth-service/src/utils/logger.js
@@ -1,22 +1,26 @@
 const winston = require('winston')
 
 const logger = winston.createLogger({
-    level:process.env.NODE_ENV  ==='production'  ? 'info' :'debug',
-    format:winston.format.combine(
-        winston.format.timestamp,
-        winston.format.errors({stack : true}),
-        winston.format.splat(),
-        winston.format.json()
+    level:process.env.NODE_ENV  ==='production'  ? 'info' :'debug',  
+    format:winston.format.combine(  // format of message
+        winston.format.timestamp,   // add timestamp everytime
+        winston.format.errors({stack : true}),  // include the stack  trace in the log entry if there is any error
+        winston.format.splat(),  // enable the support for message template
+        winston.format.json() 
     ),
-    defaultMeta:{service:'auth-service'},
-    transports:[
+    defaultMeta:{service:'auth-service'},   // this is meta data
+    // Define where logs should be sent
+    transports:[   
+         // Log to console (terminal)
         new winston.transport.Console({
             format:winston.format.combine(
-                winston.format.colorize(),
-                winston.format.simple()
+                winston.format.colorize(), // Colorize log levels in console
+                winston.format.simple() // Simple text format for console output
             ),
         }),
+                // Log errors to a file named 'error.log'
         new winston.transport.File({filename:'error.log',level:'error'}),  // if there is any error it log in auth-service 
+         // Log all messages (info, warn, error, debug, etc.) to 'combined.log'
         new winston.transport.File({filename:"combined.log"}) // this is the file create all combine error
     ]
 })
