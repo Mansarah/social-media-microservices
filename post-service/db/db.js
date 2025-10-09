0582e17b0807a501843c36c3bfa2d93f1033f525
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-10-09 23:37:10 +0530

    `feat:post-service/db connection and server js , rabbit mq is pending`

---
+++ post-service/db/db.js
@@ -0,0 +1,16 @@
+const mongoose= require('mongoose')
+const logger = require('../src/utils/logger')
+
+
+const connectToPostDB = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI_POST)
+        logger.info("Connected to PostDB")
+    } catch (error) {
+        logger('Error while connection PostDb',error)
+        process.exit(1)
+        
+    }
+}
+
+module.exports = connectToPostDB
