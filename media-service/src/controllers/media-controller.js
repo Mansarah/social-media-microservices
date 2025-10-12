bebe202b128170e5735e9ff905a4677a2454823c
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2025-10-12 10:18:05 +0530

    `feat: media-service/cloduinaryUtils and upload media controller`

---
+++ media-service/src/controllers/media-controller.js
@@ -0,0 +1,58 @@
+const Media = require("../models/Media")
+const { uploadMediaToCloudinary } = require("../utils/cloudinaryUtils")
+const logger = require("../utils/logger")
+
+
+
+
+
+const uploadMedia = async (req,res)=>{
+    logger.info("upload media endpoint hit")
+    try {
+
+        if(!req){
+            logger.error("No file found. Please add a file and try again!")
+            return res.status(400).json({
+                success:false,
+                message:"No file found.Please add a file and try again!"
+            })
+        }
+
+        const {originalName, mimeType , buffer} = req.body
+        const userId = req.user.userId
+
+        logger.info(`File details name=${originalName},type=${mimeType}`)
+        logger.info('Uploading to cloudniary starting...')
+
+
+        const cloudinaryUploadResult = await uploadMediaToCloudinary(req.file)
+        logger.info(`Cloduinary upload succesfully. Public Id
+            : - ${cloudinaryUploadResult.public_id}`)
+
+            const newlyCreatedMedia = new Media({
+                publicId:cloudinaryUploadResult.public_id,
+                originalName:originalName,
+                mimeType:mimeType,
+                url:cloudinaryUploadResult.secure_url,
+                userId
+            })
+
+            await newlyCreatedMedia.save()
+
+            res.status(201).json({
+                success:true,
+                medaiId :newlyCreatedMedia._id,
+                url:newlyCreatedMedia.url,
+                message:"Media Upload is successfully"
+            })
+    } catch (error) {
+        logger.error('Error creating media',error)
+        res.status(500).json({
+            success:false,
+            message:"Error creating media"
+        })
+        
+    }
+}
+
+
---
+++ media-service/src/utils/cloudinaryUtils.js
@@ -0,0 +1,23 @@
+const logger = require('./logger')
+const cloudinary = require('../config/cloudinary')
+
+const uploadMediaToCloudinary = (file)=>{
+    return new Promise((resolve,reject)=>{
+        const uploadStream = cloudinary.uploader.upload_stream({
+            resource_type:"auto",
+        },
+        (error,result)=>{
+            if(error){
+                logger.error("Error while uploading media to cloudinary")
+                reject(error)
+            }else{
+                resolve(result)
+            }
+        }
+    )
+    uploadStream.end(file.buffer)
+    })
+}
+
+
+module.exports = {uploadMediaToCloudinary}
